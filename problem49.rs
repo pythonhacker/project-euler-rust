1fd0976b83287bb4534cdab1bd2178465c4005c8
Anand <anand@zoko.io>
2020-06-13 17:48:11 +0530

    Replace _32 => _u32

--- problem49.rs
+++ problem49.rs
@@ -5,7 +5,7 @@ extern crate itertools;
 use itertools::Itertools;
 
 mod common;
-use common::{vector_to_digit_32, digit_to_vector_32, is_prime_32};
+use common::{vector_to_digit_u32, digit_to_vector_u32, is_prime_u32};
 
 fn prime_permute() -> String {
 
@@ -13,17 +13,17 @@ fn prime_permute() -> String {
     let mut count: usize = 0;
     
     loop {
-        if is_prime_32(n) {
-            let n_vec: Vec<u32> = digit_to_vector_32(n);
+        if is_prime_u32(n) {
+            let n_vec: Vec<u32> = digit_to_vector_u32(n);
             let mut n_prime: Vec<u32> = vec![n];
             let mut idx: usize = 0;
             
             for it in n_vec.iter().permutations(4) {
                 if *it[0] == 0 { continue; }
 
-                let mut n_perm: u32 = vector_to_digit_32(it.to_vec());
+                let mut n_perm: u32 = vector_to_digit_u32(it.to_vec());
                 
-                if is_prime_32(n_perm) && (n_perm > n_prime[idx]) && ((n_perm - n_prime[idx]) == 3330) {
+                if is_prime_u32(n_perm) && (n_perm > n_prime[idx]) && ((n_perm - n_prime[idx]) == 3330) {
                     n_prime.push(n_perm);
                     idx += 1;
                 }
