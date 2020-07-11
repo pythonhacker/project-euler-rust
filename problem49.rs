980ea09876e825667efa1e14ae2b8cfb5de155a9
Anand <anand@zoko.io>
2020-07-12 00:34:33 +0530

    Use vectorp_to_digit_u32

--- problem49.rs
+++ problem49.rs
@@ -5,7 +5,7 @@ extern crate itertools;
 use itertools::Itertools;
 
 mod common;
-use common::{vector_to_digit_u32, digit_to_vector_u32, is_prime_u32};
+use common::{vectorp_to_digit_u32, digit_to_vector_u32, is_prime_u32};
 
 fn prime_permute() -> String {
 
@@ -21,7 +21,7 @@ fn prime_permute() -> String {
             for it in n_vec.iter().permutations(4) {
                 if *it[0] == 0 { continue; }
 
-                let mut n_perm: u32 = vector_to_digit_u32(it.to_vec());
+                let mut n_perm: u32 = vectorp_to_digit_u32(it.to_vec());
                 
                 if is_prime_u32(n_perm) && (n_perm > n_prime[idx]) && ((n_perm - n_prime[idx]) == 3330) {
                     n_prime.push(n_perm);
